3d5ef07cf9f053c40ffa1c0b2dfb541dbd0cb6c2
challa-bhagyalakkshmi <challabhagyalakshmi2298@gmail.com>
2022-07-13 23:28:13 +0530

    added folder

---
+++ src/main/java/mavenbuildapp/Amazon.java
@@ -0,0 +1,10 @@
+package mavenbuildapp;
+
+public class Amazon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is a program");
+	}
+
+}
